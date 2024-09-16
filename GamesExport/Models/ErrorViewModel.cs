f32961d205a0cf677d42943689b043cccf009ff0
WILLIAM TAVARES <112444968+MestreWil@users.noreply.github.com>
2024-09-16 20:34:20 -0300

    sou foda

---
+++ GamesExport/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace GamesExport.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
