bb264d2d1f2cebed2ac72bc344eeab6f8c26b80d
WILLIAM TAVARES <112444968+MestreWil@users.noreply.github.com>
2024-09-16 21:40:05 -0300

    atualizamos o bootstrap

--- GamesExport/Controllers/HomeController.cs
+++ GamesExport/Controllers/HomeController.cs
@@ -15,6 +15,7 @@ namespace GamesExport.Controllers
 
         public IActionResult Index()
         {
+            ViewData["email"] = "williamtavaresdemoura@gmail.com";
             return View();
         }
 
